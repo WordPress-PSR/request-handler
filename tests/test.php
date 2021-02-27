d445cb4dcd189bcad3172fff299de13b8833da97
David Stone <david@nnucomputerwhiz.com>
2021-02-27 02:17:41 -0700

    Add first test

--- tests/test.php
+++ tests/test.php
@@ -1,6 +1,21 @@
 <?php
 
+use Tgc\WordPressPsr\Psr17\Psr17Factory;
+use Tgc\WordPressPsr\Psr17\Psr17FactoryProvider;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $request_handler = \Tgc\WordPressPsr\RequestHandlerFactory::create( '../wordpress' );
-$request_handler->handle( \Tgc\WordPressPsr\RequestHandlerFactory: );
+
+$psr17FactoryProvider = new Psr17FactoryProvider();
+
+/** @var Psr17Factory $psr17factory */
+foreach ($psr17FactoryProvider->getFactories() as $psr17factory) {
+	if ($psr17factory::isServerRequestCreatorAvailable()) {
+		$request_creator =  $psr17factory::getServerRequestCreator();
+	}
+}
+
+$request = $request_creator->createServerRequestFromGlobals();
+$response = $request_handler->handle( $request );
+echo $response->getBody()->getContents();
