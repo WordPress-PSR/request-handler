681d9c49eb1f618fff7a804ddd0f8efaad469099
David Stone <david@nnucomputerwhiz.com>
2026-03-26 21:27:30 -0600

    feat: update dependencies and source for PHP 8.1+ compatibility (#11)
    - Bump minimum PHP requirement from >=7.1 to >=8.1
    - Remove symfony/polyfill-php80 (no longer needed on PHP 8.1+)
    - Update rector/rector from dev-main to ^2.0
    - Update friendsofphp/php-cs-fixer from ^2.18.2 to ^3.0
    - Update phpstan/phpstan from ^0.12.80 to ^2.0
    - Update phpunit/phpunit from ^9.3.8 to ^10.0
    - Update wp-coding-standards/wpcs from ^2.3.0 to ^3.0
    - Update dealerdirect/phpcodesniffer-composer-installer from ^0.7.0 to ^1.0
    - Migrate rector.php from ContainerConfigurator to RectorConfig API
    - Fix Rector rules: update AbstractRector namespace from
      Rector\Core\Rector to Rector\Rector (Rector 1.0+ API)
    - Update phpcs.xml testVersion from 7.2- to 8.1-
    
    Closes #4

--- rector.php
+++ rector.php
@@ -2,65 +2,41 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/wordpress/' );
 	define( 'WPINC', 'wp-includes' );
 	define( 'EP_NONE', 0 );
 }
-return static function ( ContainerConfigurator $container_configurator ): void {
-	// get parameters
-	$parameters = $container_configurator->parameters();
-	  $parameters->set(Option::SETS, [
-	      SetList::CODE_QUALITY,
-	      SetList::PHP_70,
-		  SetList::PHP_71,
-		  SetList::PHP_72,
-		  SetList::PHP_73,
-		  SetList::PHP_74,
-		  SetList::PHP_80
-	  ]);
-	$parameters->set(
-		Option::AUTOLOAD_PATHS,
-		array(
-			__DIR__ . '/wordpress',
-		)
-	);
-	// get services (needed for register a single rule)
-	$services = $container_configurator->services();
-	// register a single rule
-	$services->set( \WordPressPsr\Rector\NoExit::class );
-	$services->set( \WordPressPsr\Rector\NewHeaderFunction::class );
-	$services->set( \WordPressPsr\Rector\NewCookieFunction::class );
-	$services->set( \WordPressPsr\Rector\NewHeaderRemoveFunction::class );
-//	$services->set( \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class );
-\Rector\Php70\Rector\ClassMethod\Php4ConstructorRector::class;
-	$parameters->set( Option::IMPORT_DOC_BLOCKS, false );
-	$parameters->set( Option::AUTO_IMPORT_NAMES, true );
-	$parameters->set(
-		Option::SKIP,
-		array(
-			__DIR__ . '/wordpress/wp-includes/SimplePie/*',
-			__DIR__ . '/wordpress/wp-includes/sodium_compat/*',
-			__DIR__ . '/wordpress/wp-includes/class-json.php',
-			__DIR__ . '/wordpress/wp-content/plugins/akismet/class.akismet-cli.php',
-			__DIR__ . '/wordpress/wp-includes/class-wp-feed-cache.php',
-			__DIR__ . '/wordpress/wp-includes/class-wp-simplepie-file.php',
-			__DIR__ . '/wordpress/wp-includes/class-wp-simplepie-sanitize-kses.php',
-			__DIR__ . '/wordpress/wp-includes/class-wp-simplepie-file.php',
-			__DIR__ . '/wordpress/wp-includes/class-wp-feed-cache.php',
-			__DIR__ . '/wordpress/wp-admin/includes/noop.php',
-		)
-	);
 
-	$parameters->set(
-		Option::PATHS,
-		array(
-			__DIR__ . '/wordpress/',
-		)
-	);
-};
+return RectorConfig::configure()
+	->withPaths( array(
+		__DIR__ . '/wordpress/',
+	) )
+	->withAutoloadPaths( array(
+		__DIR__ . '/wordpress',
+	) )
+	->withSets( array(
+		SetList::CODE_QUALITY,
+		LevelSetList::UP_TO_PHP_82,
+	) )
+	->withRules( array(
+		\WordPressPsr\Rector\NoExit::class,
+		\WordPressPsr\Rector\NewHeaderFunction::class,
+		\WordPressPsr\Rector\NewCookieFunction::class,
+		\WordPressPsr\Rector\NewHeaderRemoveFunction::class,
+	) )
+	->withImportNames( importDocBlockNames: false )
+	->withSkip( array(
+		__DIR__ . '/wordpress/wp-includes/SimplePie/*',
+		__DIR__ . '/wordpress/wp-includes/sodium_compat/*',
+		__DIR__ . '/wordpress/wp-includes/class-json.php',
+		__DIR__ . '/wordpress/wp-content/plugins/akismet/class.akismet-cli.php',
+		__DIR__ . '/wordpress/wp-includes/class-wp-feed-cache.php',
+		__DIR__ . '/wordpress/wp-includes/class-wp-simplepie-file.php',
+		__DIR__ . '/wordpress/wp-includes/class-wp-simplepie-sanitize-kses.php',
+		__DIR__ . '/wordpress/wp-admin/includes/noop.php',
+	) );
